5947a4f7eaa0f616abc35ad0d9f46a061c771c3a
Patrick <g00409592@atu.ie>
2023-03-29 23:27:40 +0100

    Add switch case with comments describing each case. Preliminary.

--- src/main/java/ie/atu/BloodDonationSystem.java
+++ src/main/java/ie/atu/BloodDonationSystem.java
@@ -18,6 +18,26 @@ public class BloodDonationSystem {
             System.out.println("Enter your choice: ");
             int userChoice = myScanner.nextInt();
             System.out.println("Choice: " + userChoice);
+
+            switch (userChoice) {
+                case 1:
+                    // New donor?
+                    break;
+                case 2:
+                    // Request donation/blood?
+                    break;
+                case 3:
+                    // Add donation. Record it to db.
+                    break;
+                case 4:
+                    // We need to also be able to view how much blood we have.
+                    break;
+                case 5:
+                    // Exit?
+                    break;
+                default:
+                    System.out.println("Input not valid.\n");
+            }
         }
     }
 }
