f9a05a6210f0c04c595b1669ba3ff27d665959a2
Patrick <g00409592@atu.ie>
2023-03-30 18:11:50 +0100

    Make constructor, getters and setters for BloodType.

---
+++ src/main/java/ie/atu/BloodType.java
@@ -0,0 +1,31 @@
+package ie.atu;
+
+public class BloodType {
+    private String bloodGroup; // A B AB or O
+    private char rhFactor; // the rhFactor can be + or -
+
+    public BloodType(String bloodGroup, char rhFactor) {
+        this.bloodGroup = bloodGroup;
+        this.rhFactor = rhFactor;
+    }
+
+    // Getter sand setters...
+
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public char getRhFactor() {
+        return rhFactor;
+    }
+
+    public void setRhFactor(char rhFactor) {
+        this.rhFactor = rhFactor;
+    }
+
+
+}
