16a3a3482986a696ec727631fd3f32b68eeecc64
Patrick <g00409592@atu.ie>
2023-03-30 23:01:01 +0100

    Add more tests for isCompatible method.
    Need to fix code of isCompatible since one test isn't giving the correct answer.

--- src/test/java/ie/atu/TestBloodType.java
+++ src/test/java/ie/atu/TestBloodType.java
@@ -17,13 +17,25 @@ public class TestBloodType {
     @Test
     void testIsCompatible() {
         BloodType bloodTypeAPlus = new BloodType("A", '+');
+        BloodType bloodTypeAMinus = new BloodType("A", '-');
+        BloodType bloodTypeBPlus = new BloodType("B", '+');
         BloodType bloodTypeBMinus = new BloodType("B", '-');
+        BloodType bloodTypeABPlus = new BloodType("AB", '+');
+        BloodType bloodTypeABMinus = new BloodType("AB", '-');
         BloodType bloodTypeOPlus = new BloodType("O", '+');
+        BloodType bloodTypeOMinus = new BloodType("O", '-');
+        // Donor - Recipient
 
-        //                                  Donor - Recipient
+        // B- --> A+
         assertFalse(BloodType.isCompatible(bloodTypeAPlus, bloodTypeBMinus));
-        // B- can't receive from A+ so assertFalse is successful.
-
-
+        // O+ --> B+
+        assertTrue(BloodType.isCompatible(bloodTypeOPlus, bloodTypeBPlus));
+        // AB- --> O-
+        assertFalse(BloodType.isCompatible(bloodTypeABMinus, bloodTypeOMinus));
+        // A- --> AB+
+        assertTrue(BloodType.isCompatible(bloodTypeAMinus, bloodTypeABPlus));
+        assertTrue(BloodType.isCompatible(bloodTypeAPlus, bloodTypeABPlus));
+        // A- --> O+
+        assertFalse(BloodType.isCompatible(bloodTypeAMinus, bloodTypeOPlus));
     }
 }
