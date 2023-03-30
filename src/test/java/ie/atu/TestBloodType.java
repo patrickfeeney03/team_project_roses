8c600b0633d47832977c76c544761b41622e8988
Patrick <g00409592@atu.ie>
2023-03-30 21:53:04 +0100

    Create test for toString of BloodType

--- src/test/java/ie/atu/TestBloodType.java
+++ src/test/java/ie/atu/TestBloodType.java
@@ -1,7 +1,19 @@
 package ie.atu;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestBloodType {
+    @Test
+    void testToString() {
+        BloodType bloodType = new BloodType("A", '+');
+        assertEquals("A+", bloodType.toString());
+    }
+
+
+
     BloodType bloodTypeA = new BloodType("A", '+');
     BloodType bloodTypeB = new BloodType("B", '+');
     BloodType bloodTypeO = new BloodType("O", '-');
+
 }
