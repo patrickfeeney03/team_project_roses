f00da5513a098fcfd2eb2d9b8a5a7b4cdd971382
Mikaela-D <G00401392@atu.ie>
2023-04-04 21:20:49 +0100

    Generated constructor for class BloodBank

--- src/main/java/ie/atu/BloodBank.java
+++ src/main/java/ie/atu/BloodBank.java
@@ -6,5 +6,12 @@ public class BloodBank {
     private String bankAddress;
     private String bankPhone;
 
+    public BloodBank(String bankID, String bankEmail, String bankAddress, String bankPhone) {
+        this.bankID = bankID;
+        this.bankEmail = bankEmail;
+        this.bankAddress = bankAddress;
+        this.bankPhone = bankPhone;
+    }
+
 
 }
