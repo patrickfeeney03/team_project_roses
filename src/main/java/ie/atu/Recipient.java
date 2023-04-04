b190f9fe900f3633a121ff9141d5312b184502fe
G00402034 <G00402034@atu.ie>
2023-04-04 20:12:15 +0100

    generated setters and getters for Recipient bloodtype

--- src/main/java/ie/atu/Recipient.java
+++ src/main/java/ie/atu/Recipient.java
@@ -5,10 +5,13 @@ public class Recipient extends User {
     private String Recipient_Disease;
     private String Transfusion_History;
 
-    public Recipient(String user_Name, String user_Address, String  user_Phone, String user_Age, String user_Id,String recipient_Disease,String transfusion_History) {
+    private String Recipient_Bloodtype;
+
+    public Recipient(String user_Name, String user_Address, String  user_Phone, String user_Age, String user_Id,String recipient_Disease,String transfusion_History,String recipient_Bloodtype) {
         super(user_Name, user_Address, user_Phone,user_Age,user_Id);
         this.Recipient_Disease = recipient_Disease;
         this.Transfusion_History = transfusion_History;
+        this.Recipient_Bloodtype = recipient_Bloodtype;
     }
 
     public String getRecipient_Disease() {
@@ -25,6 +28,14 @@ public class Recipient extends User {
 
     public void setTransfusion_History(String transfusion_History) {
         Transfusion_History = transfusion_History;
+    }
+
+    public String getRecipient_Bloodtype() {
+        return Recipient_Bloodtype;
+    }
+
+    public void setRecipient_Bloodtype(String recipient_Bloodtype) {
+        Recipient_Bloodtype = recipient_Bloodtype;
     }
 }
 
