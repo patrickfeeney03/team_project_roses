4f690ce98a1709de90588d7fec7e12642658ad2a
G00402034 <G00402034@atu.ie>
2023-04-04 20:56:49 +0100

    changed variable names to lowercase and fixed getters and setters

--- src/main/java/ie/atu/Recipient.java
+++ src/main/java/ie/atu/Recipient.java
@@ -2,40 +2,40 @@ package ie.atu;
 
 public class Recipient extends User {
 
-    private String Recipient_Disease;
-    private String Transfusion_History;
+    private String recipient_Disease;
+    private String transfusion_History;
 
-    private String Recipient_Bloodtype;
+    private String recipient_Bloodtype;
 
     public Recipient(String user_Name, String user_Address, String  user_Phone, String user_Age, String user_Id,String recipient_Disease,String transfusion_History,String recipient_Bloodtype) {
         super(user_Name, user_Address, user_Phone,user_Age,user_Id);
-        this.Recipient_Disease = recipient_Disease;
-        this.Transfusion_History = transfusion_History;
-        this.Recipient_Bloodtype = recipient_Bloodtype;
+        this.recipient_Disease = recipient_Disease;
+        this.transfusion_History = transfusion_History;
+        this.recipient_Bloodtype = recipient_Bloodtype;
     }
 
     public String getRecipient_Disease() {
-        return Recipient_Disease;
+        return recipient_Disease;
     }
 
     public void setRecipient_Disease(String recipient_Disease) {
-        Recipient_Disease = recipient_Disease;
+        this.recipient_Disease = recipient_Disease;
     }
 
     public String getTransfusion_History() {
-        return Transfusion_History;
+        return transfusion_History;
     }
 
     public void setTransfusion_History(String transfusion_History) {
-        Transfusion_History = transfusion_History;
+        this.transfusion_History = transfusion_History;
     }
 
     public String getRecipient_Bloodtype() {
-        return Recipient_Bloodtype;
+        return recipient_Bloodtype;
     }
 
     public void setRecipient_Bloodtype(String recipient_Bloodtype) {
-        Recipient_Bloodtype = recipient_Bloodtype;
+        this.recipient_Bloodtype = recipient_Bloodtype;
     }
 }
 
