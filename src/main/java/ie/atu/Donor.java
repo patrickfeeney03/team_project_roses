74bbd4da23892f5012d7eb35d62a3aafff5f0705
Jakub Mamala <g00408184@atu.ie>
2023-04-05 18:17:33 +0100

    Set up private variables

--- src/main/java/ie/atu/Donor.java
+++ src/main/java/ie/atu/Donor.java
@@ -1,7 +1,17 @@
 package ie.atu;
 
+import java.sql.SQLOutput;
+
+
 public class Donor extends User {
     private String bloodType;
-
+    private String Last_Med;
+    private String First_Med;
+    public Donor(String First_Med, String Last_Med, String User_Name, String user_Address, String  user_Phone, String user_Age, String user_Id ,String bloodType) {
+        super(User_Name, user_Address, user_Phone,user_Age,user_Id);
+        this.First_Med = First_Med;
+        this.Last_Med = Last_Med;
+        this.bloodType = bloodType;
+    }
 
 }
