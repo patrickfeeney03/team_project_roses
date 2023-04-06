0ffbc34b3c20a65563718f7f823f1c7f51bdccad
alan-hynes <g00400498@atu.ie>
2023-04-06 16:34:56 +0100

    BloodStock class with getters and setters

--- src/main/java/ie/atu/BloodStock.java
+++ src/main/java/ie/atu/BloodStock.java
@@ -3,5 +3,22 @@ package ie.atu;
 public class BloodStock {
     private BloodType bloodGroup;
     private int amount;
+
+    //getters and setters
+    public BloodType getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(BloodType bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
 
