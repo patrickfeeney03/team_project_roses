202352c6944a0f41050491d184dc09079733344d
Mikaela-D <G00401392@atu.ie>
2023-04-13 11:01:26 +0100

    Added a string with the command to be executed in mySQL

--- src/main/java/ie/atu/DBSelect.java
+++ src/main/java/ie/atu/DBSelect.java
@@ -1,4 +1,10 @@
 package ie.atu;
 
 public class DBSelect {
+    public static void main(String[] args) {
+        String selectSQL = "SELECT u.patientFirstName, u.patientLastName, e.patientDisease " +
+                "FROM patient_info u " +
+                "JOIN patient_medical_data e ON u.patientID = e.patientID";
+
+    }
 }
