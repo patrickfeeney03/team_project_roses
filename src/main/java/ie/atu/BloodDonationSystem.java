9130b0710d90dfcd473f78cd20fed64fb95a4069
Patrick <g00409592@atu.ie>
2023-04-13 19:28:54 +0100

    Add switch case for register, login or exit main app.

--- src/main/java/ie/atu/BloodDonationSystem.java
+++ src/main/java/ie/atu/BloodDonationSystem.java
@@ -8,11 +8,14 @@ public class BloodDonationSystem {
 
         BloodType bloodType = new BloodType("B", '-');
 
+        String userName = "admin";
+        String userPassword = "admin";
+
         // Main loop
         boolean exit = false;
         while (!exit) {
             System.out.println("\nMain Menu:");
-            System.out.println("1. Register");
+            System.out.println("1. Register (not working yet)");
             System.out.println("2. Login");
             System.out.println("3. Exit");
 
@@ -20,24 +23,33 @@ public class BloodDonationSystem {
             System.out.println("Enter your choice: ");
             userChoice = myScanner.nextInt();
 
-            System.out.println("1: Request Blood\n2: Record Donation\n3: View Stock");
-            userChoice = myScanner.nextInt();
-
             switch (userChoice) {
+
                 case 1:
-                    // New donor?
+                    System.out.println("'executing registering process' (not working yet)");
                     break;
                 case 2:
-                    // Request donation/blood?
+                    System.out.println("1: Request Blood\n2: Record Donation\n3: View Stock");
+                    userChoice = myScanner.nextInt();
+
+                    switch (userChoice) {
+                        case 1:
+                            requestBlood();
+                            break;
+                        case 2:
+                            recordDonation();
+                            break;
+                        case 3:
+                            viewStock();
+                            break;
+                        default:
+                            System.out.println("Input not valid.\n");
+                    }
                     break;
                 case 3:
-                    // Add donation. Record it to db.
+                    exit = true;
                     break;
-                case 4:
-                    // We need to also be able to view how much blood we have.
-                    break;
-                default:
-                    System.out.println("Input not valid.\n");
+
             }
         }
     }
