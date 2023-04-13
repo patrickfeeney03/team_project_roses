1cd30169a3645165c56ed2817ee834038e69b4ee
Mikaela-D <G00401392@atu.ie>
2023-04-13 11:03:33 +0100

    Added a try catch to the DBSelect code

--- src/main/java/ie/atu/DBSelect.java
+++ src/main/java/ie/atu/DBSelect.java
@@ -1,10 +1,25 @@
 package ie.atu;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DBSelect {
     public static void main(String[] args) {
         String selectSQL = "SELECT u.patientFirstName, u.patientLastName, e.patientDisease " +
                 "FROM patient_info u " +
                 "JOIN patient_medical_data e ON u.patientID = e.patientID";
 
+        try (Connection connection = DBConnectionUtils.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(selectSQL)) {
+
+            while (resultSet.next()) {
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
