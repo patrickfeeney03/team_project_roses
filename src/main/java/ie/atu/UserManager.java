985d9bff17a733b9ec99e065a625f88f83496a8b
Patrick <g00409592@atu.ie>
2023-04-14 23:43:40 +0100

    Add prepared statement for id

--- src/main/java/ie/atu/UserManager.java
+++ src/main/java/ie/atu/UserManager.java
@@ -6,4 +6,17 @@ public class UserManager {
     public Connection getConnection() throws SQLException {
         return DBConnectionUtils.getConnection();
     }
+
+    public boolean addUser(User user) {
+        String insertSQL = "INSERT INTO user (userID, email, password, name, role, address, phone, age) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        try (Connection connection = getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(insertSQL)) {
+            preparedStatement.setInt(1, user.getUser_Id());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
