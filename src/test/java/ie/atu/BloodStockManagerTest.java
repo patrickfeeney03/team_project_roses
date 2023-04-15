f65d71c4c01bc1ce3397307385489ad9aaa58f40
Patrick <g00409592@atu.ie>
2023-04-15 14:36:44 +0100

    Add small test to test the functionality of the getAvailableBloodStock method.
    The method needs to be called on an instance of BloodStockManager and be passed a BloodType object. The BloodType object will have a blood group and a rh factor.

--- src/test/java/ie/atu/BloodStockManagerTest.java
+++ src/test/java/ie/atu/BloodStockManagerTest.java
@@ -1,12 +1,23 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class BloodStockManagerTest {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         BloodStockManager myBloodManager = new BloodStockManager();
         BloodType exampleType = new BloodType("A", '+');
 
         int amount = myBloodManager.getAvailableBloodstock(exampleType);
         System.out.println("The amount of blood is: " + amount);
+
+        System.out.println("Get available blood of what group: ");
+        String bloodGroup = scanner.next();
+        System.out.println("Rh factor: ");
+        char rhFactor = scanner.next().charAt(0);
+        BloodType exampleType2 = new BloodType(bloodGroup, rhFactor);
+        int amount2 = myBloodManager.getAvailableBloodstock(exampleType2);
+        System.out.println("The amount second amount of blood is: " + amount2);
 
     }
 }
