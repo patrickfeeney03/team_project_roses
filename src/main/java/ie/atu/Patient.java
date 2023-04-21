2c278e9855c6c5b8da050d066215589a564d077a
Mikaela-D <G00401392@atu.ie>
2023-04-21 22:56:19 +0100

    Generated variables for the patient class

--- src/main/java/ie/atu/Patient.java
+++ src/main/java/ie/atu/Patient.java
@@ -1,4 +1,13 @@
 package ie.atu;
 
 public class Patient {
+    private int patient_Id;
+    private String patient_email;
+    private String patient_password;
+    private String patient_firstName;
+    private String patient_lastName;
+    private String patient_address;
+    private String patient_phone;
+    private String patient_emergencyPhone;
+    private int patient_age;
 }
--- src/test/java/ie/atu/PatientManagerTest.java
+++ src/test/java/ie/atu/PatientManagerTest.java
@@ -3,5 +3,6 @@ package ie.atu;
 public class PatientManagerTest {
     public static void main(String[] args) {
         PatientManager PatientManager = new PatientManager();
+
     }
 }
