4bcab3755369bb5477bbb6930b19637305ee8c55
Patrick <g00409592@atu.ie>
2023-04-21 20:37:29 +0100

    Create test that uses the new constructor in Donation.
    This new constructor uses the following parameters:
    donor, bloodbank, and unitsDonated.

--- src/test/java/ie/atu/UseBloodBankTest.java
+++ src/test/java/ie/atu/UseBloodBankTest.java
@@ -1,13 +1,22 @@
 package ie.atu;
 
 public class UseBloodBankTest {
-    /*
-     So, before performing a donation, the bloodBank details will be already defined, since
-     a blood bank is a place, which doesn't really change, except if the nurse (user)
-     moves to a different location.
-     */
-    BloodBank bloodBank = new BloodBank("2", "email", "address",
-            "thisIsAPhoneNumber");
-    //Donation donation = new Donation()
+    public static void main(String[] args) {
+        /*
+        So, before performing a donation, the bloodBank details will be already defined, since
+        a blood bank is a place, which doesn't really change, except if the nurse (user)
+        moves to a different location.
+        */
+        BloodBank bloodBank = new BloodBank("2", "email", "address",
+                "thisIsAPhoneNumber");
+        BloodType bloodType = new BloodType("A", '+');
+        Donor donor = new Donor(2, "email", "password", "name",
+                "noRole", "123Address", "999", 20, "Yesterday",
+                "Today", bloodType.toString());
+        Donation donation = new Donation(donor, bloodBank, 1);
+        System.out.println(donation);
+    }
+
+
 
 }
