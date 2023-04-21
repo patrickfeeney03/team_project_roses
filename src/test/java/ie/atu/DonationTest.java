cf54a45a29840e39e60781a7397112c48556a8e4
Patrick <g00409592@atu.ie>
2023-04-21 09:57:07 +0100

    Create main for DonationTest

--- src/test/java/ie/atu/DonationTest.java
+++ src/test/java/ie/atu/DonationTest.java
@@ -1,4 +1,12 @@
 package ie.atu;
 
 public class DonationTest {
+    public static void main(String[] args) {
+        // Use will input the info for populating the bloodType
+        BloodType bloodType1 = new BloodType("A", '+');
+        int donatedAmount1 = 3;
+        //Donation donation1 = new Donation();
+    }
+
+
 }
