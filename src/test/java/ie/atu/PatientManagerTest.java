76bd6d27ff2c06ac53130a93341aa5660f4c7766
Mikaela-D <G00401392@atu.ie>
2023-04-21 23:04:57 +0100

    Initialised properties in object Patient that are going to be used for testing

--- src/test/java/ie/atu/PatientManagerTest.java
+++ src/test/java/ie/atu/PatientManagerTest.java
@@ -3,6 +3,8 @@ package ie.atu;
 public class PatientManagerTest {
     public static void main(String[] args) {
         PatientManager PatientManager = new PatientManager();
-
+        Patient patientTest = new Patient(22, "some@email.com", "password03", "Sean",
+                "Conroy", "someStreet09", "353 89 000 0000", "353 89 100 0000",
+                55);
     }
 }
