e8963f4162a09a882ab75fd097f4be7e00adbff4
Mikaela-D <G00401392@atu.ie>
2023-04-21 23:26:39 +0100

    Added code to add a patient in PatientManager

--- src/main/java/ie/atu/PatientManager.java
+++ src/main/java/ie/atu/PatientManager.java
@@ -2,10 +2,28 @@ package ie.atu;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class PatientManager {
 
     public Connection getConnection() throws SQLException {
         return DBConnectionUtils.getConnection();
+    }
+
+    public void register(Scanner scanner) {
+        System.out.print("Enter patient email: ");
+        String patient_email = scanner.next();
+        System.out.print("Enter patient's first name: ");
+        String patient_firstName = scanner.next();
+        System.out.print("Enter patient's last name: ");
+        String patient_lastName = scanner.next();
+        System.out.print("Enter patient's address: ");
+        String patient_address = scanner.next();
+        System.out.print("Enter patient's phone: ");
+        String patient_phone = scanner.next();
+        System.out.print("Enter patient's emergency phone: ");
+        String patient_emergencyPhone = scanner.next();
+        System.out.print("Enter patient's age: ");
+        int patient_age = scanner.nextInt();
     }
 }
