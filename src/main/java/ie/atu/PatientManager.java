8ea471ad1643c399f51a64eb14e1c96c1c9b9654
Mikaela-D <G00401392@atu.ie>
2023-04-21 23:14:13 +0100

    Added code to connect to the database in PatientManager class

--- src/main/java/ie/atu/PatientManager.java
+++ src/main/java/ie/atu/PatientManager.java
@@ -1,4 +1,11 @@
 package ie.atu;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class PatientManager {
+
+    public Connection getConnection() throws SQLException {
+        return DBConnectionUtils.getConnection();
+    }
 }
