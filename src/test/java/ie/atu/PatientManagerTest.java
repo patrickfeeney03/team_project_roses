d2dce17e8460790dec877aad04597ecb7c0381a9
Mikaela-D <G00401392@atu.ie>
2023-04-21 23:10:29 +0100

    Added code to test if addPatient() was successful

--- src/test/java/ie/atu/PatientManagerTest.java
+++ src/test/java/ie/atu/PatientManagerTest.java
@@ -2,9 +2,16 @@ package ie.atu;
 
 public class PatientManagerTest {
     public static void main(String[] args) {
+        // Create PatientManager object
         PatientManager PatientManager = new PatientManager();
+
+        // Initialise properties in the Patient class
         Patient patientTest = new Patient(22, "some@email.com", "password03", "Sean",
                 "Conroy", "someStreet09", "353 89 000 0000", "353 89 100 0000",
                 55);
+
+        // Test if addPatient was successful
+        boolean addPatientResult = PatientManager.addPatient(patientTest);
+        System.out.println("addUser result: " + addPatientResult);
     }
 }
