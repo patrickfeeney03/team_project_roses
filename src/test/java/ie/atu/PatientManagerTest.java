fcfd82ea971a8682db6f962ddd8b2a457c2f3b8f
Mikaela-D <G00401392@atu.ie>
2023-04-22 13:22:58 +0100

    Changed variable's letter case PatientManager to patientManager

--- src/test/java/ie/atu/PatientManagerTest.java
+++ src/test/java/ie/atu/PatientManagerTest.java
@@ -3,7 +3,7 @@ package ie.atu;
 public class PatientManagerTest {
     public static void main(String[] args) {
         // Create PatientManager object
-        PatientManager PatientManager = new PatientManager();
+        PatientManager patientManager = new PatientManager();
 
         // Initialise properties in the Patient class
         Patient patientTest = new Patient(100, "Sean", "Conroy", 55,
@@ -11,11 +11,11 @@ public class PatientManagerTest {
                 "353 67 555 5555", "353 444 4444");
 
         // Test if addPatient was successful
-        boolean addPatientResult = PatientManager.addPatient(patientTest);
+        boolean addPatientResult = patientManager.addPatient(patientTest);
         System.out.println("addPatient result: " + addPatientResult);
 
         // Test if removePatient was successful
-        boolean removePatientResult = PatientManager.removePatient(patientTest);
+        boolean removePatientResult = patientManager.removePatient(patientTest);
         System.out.println("removePatient result: " + removePatientResult);
 
         // Test for updateUser()
