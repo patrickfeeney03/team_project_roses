369e0b838b6e29943be232c1f4680cfe899bafbd
Mikaela-D <G00401392@atu.ie>
2023-04-22 16:16:44 +0100

    Added code to print each detail of the patient information

--- src/main/java/ie/atu/PatientManager.java
+++ src/main/java/ie/atu/PatientManager.java
@@ -177,7 +177,15 @@ public class PatientManager {
                 patient.setPatient_phone(resultSet.getString("patientPhone"));
                 patient.setPatient_emergencyPhone(resultSet.getString("patientEmergencyPhone"));
 
-
+                System.out.println("Patient ID: " + patient.getPatient_Id());
+                System.out.println("Patient First Name: " + patient.getPatient_firstName());
+                System.out.println("Patient Last Name: " + patient.getPatient_lastName());
+                System.out.println("Patient Age: " + patient.getPatient_age());
+                System.out.println("Patient DOB: " + patient.getPatient_DOB());
+                System.out.println("Patient Email: " + patient.getPatient_email());
+                System.out.println("Patient Address: " + patient.getPatient_address());
+                System.out.println("Patient Phone: " + patient.getPatient_phone());
+                System.out.println("Patient Emergency Phone: " + patient.getPatient_emergencyPhone());
             }
         } catch (SQLException e) {
             e.printStackTrace();
