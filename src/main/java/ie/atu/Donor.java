e8898bbe2e32536181057365f3f033aaf28dde79
Jakub Mamala <G00408184@atu.ie>
2023-04-22 20:50:32 +0100

    Changed Donor from extending User to extending Patient

--- src/main/java/ie/atu/Donor.java
+++ src/main/java/ie/atu/Donor.java
@@ -1,13 +1,11 @@
 package ie.atu;
 
-public class Donor extends User {
+public class Donor extends Patient {
     private String bloodType;
     private String Last_Med;
     private String First_Med;
-    public Donor(int user_Id, String user_email, String user_password, String user_Name, String user_role,
-                 String user_Address, String user_Phone, int user_Age, String First_Med, String Last_Med,
-                 String bloodType) {
-        super(user_Id, user_email, user_password, user_Name, user_role, user_Address, user_Phone, user_Age);
+    public Donor(int patient_Id, String patient_firstName, String patient_lastName, int patient_age, String patient_DOB, String patient_email, String patient_address, String patient_phone, String patient_emergencyPhone, String First_Med, String Last_Med, String bloodType) {
+        super(patient_Id, patient_firstName, patient_lastName, patient_age, patient_DOB, patient_email, patient_address, patient_phone, patient_emergencyPhone);
         this.First_Med = First_Med;
         this.Last_Med = Last_Med;
         this.bloodType = bloodType;
