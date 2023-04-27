8db6cff6107403c953019102e8b556ea232e5a5a
Patrick <g00409592@atu.ie>
2023-04-27 18:16:26 +0100

    Get rid of unnecessary variables inside of Donor. Only leave BloodType inside of it.

--- src/main/java/ie/atu/Donor.java
+++ src/main/java/ie/atu/Donor.java
@@ -1,46 +1,21 @@
 package ie.atu;
 
 public class Donor extends Patient {
-    private String bloodType;
-    private String Last_Med;
-    private String First_Med;
-    public Donor(int patient_Id, String patient_firstName, String patient_lastName, int patient_age, String patient_DOB, String patient_email, String patient_address, String patient_phone, String patient_emergencyPhone, String First_Med, String Last_Med, String bloodType) {
-        super(patient_Id, patient_firstName, patient_lastName, patient_age, patient_DOB, patient_email, patient_address, patient_phone, patient_emergencyPhone);
-        this.First_Med = First_Med;
-        this.Last_Med = Last_Med;
+    private BloodType bloodType;
+
+    public Donor(int patient_Id, String patient_firstName, String patient_lastName, int patient_age, String patient_DOB,
+                 String patient_email, String patient_address, String patient_phone, String patient_emergencyPhone,
+                 BloodType bloodType) {
+        super(patient_Id, patient_firstName, patient_lastName, patient_age, patient_DOB, patient_email, patient_address,
+                patient_phone, patient_emergencyPhone);
         this.bloodType = bloodType;
     }
 
-    public Donor(String bloodType) {
-        this.bloodType = bloodType;
-        this.Last_Med = "Yesterday";
-        this.First_Med = "A year ago";
-    }
-
-
-
-    public String getBloodType() {
+    public BloodType getBloodType() {
         return bloodType;
     }
 
-    public void setBloodType(String bloodType) {
+    public void setBloodType(BloodType bloodType) {
         this.bloodType = bloodType;
     }
-    public String getLast_Med() {
-        return Last_Med;
-    }
-
-    public void setLast_Med(String Last_Med) {
-        this.Last_Med = Last_Med;
-    }
-    public String getFirst_Med() {
-        return First_Med;
-    }
-
-    public void setFirst_Med(String First_Med) {
-        this.First_Med = First_Med;
-    }
-
-
-
 }
--- src/test/java/ie/atu/DonationTest.java
+++ src/test/java/ie/atu/DonationTest.java
@@ -4,7 +4,7 @@ public class DonationTest {
     public static void main(String[] args) {
         // Use will input the info for populating the bloodType
         BloodType bloodType1 = new BloodType("A", '+');
-        Donor donor1 = new Donor(2, "email", "password", "name",
+        Donor donor1 = new Donor(0, "email", "password", "name",
                 "noRole", "123Address", "999", 20, "Yesterday",
                 "Today", bloodType1.toString());
         int donatedAmount1 = 3;
@@ -16,12 +16,12 @@ public class DonationTest {
 
         Donation donation2  = new Donation(donor1, donatedAmount1);
         System.out.println("Some of Donor's info\n" +
-                "Name: " + donor1.getUser_name() +
-                "\nAge: " + donor1.getUser_age() +
-                "\nFirst visit: " + donor1.getFirst_Med());
+                "Name: " + donor1.getPatient_firstName() +
+                "\nAge: " + donor1.getPatient_age() +
+                "\nFirst visit: " + donor1.getRegister_date());
         System.out.println("\nDonated amount: " + donatedAmount1 + " units.");
         System.out.println("Donated amount info from donation2: " + donation2.getUnitsDonated());
-        System.out.println("\nBloodType info: " + bloodType1.toString());
+        //System.out.println("\nBloodType info: " + bloodType1.toString());
         System.out.println("BloodType info from donor1: " + donor1.getBloodType().toString());
         System.out.println("BloodType info from donation2: " + donation2.getDonor().getBloodType().toString());
     }
