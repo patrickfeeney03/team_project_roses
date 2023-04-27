a37ee811bc3c28015ff22ba9aa2b641555e4ff1c
Patrick <g00409592@atu.ie>
2023-04-27 18:30:53 +0100

    Get rid of unused variables in Recipient class and update the constructor.

--- src/main/java/ie/atu/Recipient.java
+++ src/main/java/ie/atu/Recipient.java
@@ -1,42 +1,22 @@
 package ie.atu;
 
 public class Recipient extends Patient {
+    private BloodType bloodType;
 
-    private String recipient_Disease;
-    private String transfusion_History;
-
-    private String recipient_Bloodtype;
-
-
-    public Recipient(int patient_Id, String patient_firstName, String patient_lastName, int patient_age, String patient_DOB, String patient_email, String patient_address, String patient_phone, String patient_emergencyPhone, String recipient_Bloodtype,String transfusion_History, String recipient_Disease) {
-        super(patient_Id, patient_firstName, patient_lastName, patient_age, patient_DOB, patient_email, patient_address, patient_phone, patient_emergencyPhone);
-        this.recipient_Disease = recipient_Disease;
-        this.transfusion_History = transfusion_History;
-        this.recipient_Bloodtype = recipient_Bloodtype;
+    public Recipient(int patient_Id, String patient_firstName, String patient_lastName, int patient_age,
+                     String patient_DOB, String patient_email, String patient_address, String patient_phone,
+                     String patient_emergencyPhone, BloodType bloodType) {
+        super(patient_Id, patient_firstName, patient_lastName, patient_age, patient_DOB, patient_email, patient_address,
+                patient_phone, patient_emergencyPhone);
+        this.bloodType = bloodType;
     }
 
-    public String getRecipient_Disease() {
-        return recipient_Disease;
+    public BloodType getBloodType() {
+        return bloodType;
     }
 
-    public void setRecipient_Disease(String recipient_Disease) {
-        this.recipient_Disease = recipient_Disease;
-    }
-
-    public String getTransfusion_History() {
-        return transfusion_History;
-    }
-
-    public void setTransfusion_History(String transfusion_History) {
-        this.transfusion_History = transfusion_History;
-    }
-
-    public String getRecipient_Bloodtype() {
-        return recipient_Bloodtype;
-    }
-
-    public void setRecipient_Bloodtype(String recipient_Bloodtype) {
-        this.recipient_Bloodtype = recipient_Bloodtype;
+    public void setBloodType(BloodType bloodType) {
+        this.bloodType = bloodType;
     }
 }
 
