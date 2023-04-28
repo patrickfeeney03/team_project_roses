2d6b1c6cde2f38acb176a2afca034745e3526787
Jakub Mamala <G00408184@atu.ie>
2023-04-28 20:23:22 +0100

    created a constructor for bloodtype and date

--- src/main/java/ie/atu/BloodUnit.java
+++ src/main/java/ie/atu/BloodUnit.java
@@ -1,4 +1,19 @@
 package ie.atu;
-
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 public class BloodUnit {
+    private BloodType bloodType;
+    private String date;
+
+    public BloodUnit(BloodType bloodType, String date) {
+        this.bloodType = bloodType;
+        this.date = date;
+    }
+
+    LocalDate currentDate = LocalDate.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            String formattedDate = currentDate.format(formatter);
+            System.out.println("Current date: " + formattedDate);
+
+
 }
