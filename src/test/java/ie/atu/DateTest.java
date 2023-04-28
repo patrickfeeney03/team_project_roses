3fef3ba408f3d415069b3ddef106d82606bca8d2
Jakub Mamala <G00408184@atu.ie>
2023-04-28 20:28:50 +0100

    tested the bloodtype and the date together

--- src/test/java/ie/atu/DateTest.java
+++ src/test/java/ie/atu/DateTest.java
@@ -5,6 +5,9 @@ public class DateTest {
 
 
         public static void main(String[] args) {
+            BloodType bloodtype = new BloodType("A",'+');
+            BloodUnit bloodUnit = new BloodUnit(bloodtype);
+            System.out.println(bloodUnit);
             LocalDate currentDate = LocalDate.now();
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
             String formattedDate = currentDate.format(formatter);
