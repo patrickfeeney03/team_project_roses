d1f9b95510f0956f997ae202105e3a8851d40b0a
Jakub Mamala <G00408184@atu.ie>
2023-04-28 21:46:57 +0100

    Created objects with Hard Code

--- src/main/java/ie/atu/Report.java
+++ src/main/java/ie/atu/Report.java
@@ -1,10 +1,22 @@
 package ie.atu;
 
 public class Report {
+
+
     public void Donor_Report(){
+        System.out.println();
+        BloodType bloodType = new BloodType("A",'+');
+        Donor donor = new Donor(0, "Mikaela", "Diaz",
+                20, "08/08/2003", "mikaelEmail", "addressMikaela",
+                "123345123", "9785684834", bloodType);
+
 
     }
     public void Recipient_Report(){
+        BloodType bloodType = new BloodType("A",'+');
+        Recipient recipient = new Recipient(0, "Mikaela", "Diaz",
+                20, "08/08/2003", "mikaelEmail", "addressMikaela",
+                "123345123", "9785684834", bloodType);
 
     }
 }
