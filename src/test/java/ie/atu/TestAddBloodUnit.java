b054a6e6ca6d060d9b7538a2086d87a50b70f391
Patrick <g00409592@atu.ie>
2023-04-29 16:38:26 +0100

    Test now automatically updates the blood_stock table.

--- src/test/java/ie/atu/TestAddBloodUnit.java
+++ src/test/java/ie/atu/TestAddBloodUnit.java
@@ -35,9 +35,27 @@ public class TestAddBloodUnit {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        /*INSERT INTO blood_units_date (blood_typesID, donorID, blood_date)
-        VALUES (1, 1, '2023-04-29');
-         */
-    }
 
+        // Update the blood_stock table
+        String updateBlood_stock = "UPDATE blood_stock bs " +
+                "LEFT JOIN ( " +
+                "SELECT blood_typesID, COUNT(*) AS count_units " +
+                "FROM blood_units_date " +
+                "GROUP BY blood_typesID " +
+                ") bud ON bs.blood_type_id = bud.blood_typesID " +
+                "SET bs.amount = COALESCE(bud.count_units, 0) " +
+                "WHERE bs.blood_type_id BETWEEN 1 AND 8;";
+
+        try (Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(updateBlood_stock)) {
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Rows affected: " + rowsAffected);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
