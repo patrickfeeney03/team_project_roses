3b01c7c5b3000037a95c5c75ecdd6919244064d7
Mikaela-D <G00401392@atu.ie>
2023-04-30 13:04:45 +0100

    Added code to connect to the MySQL database

--- src/main/java/ie/atu/BloodBankManager.java
+++ src/main/java/ie/atu/BloodBankManager.java
@@ -1,5 +1,18 @@
 package ie.atu;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class BloodBankManager {
+
+    // here there will be two methods
+    // 1 getBloodBankByID
+    // 2 printBloodBanks
+
+    public Connection getConnection() throws SQLException {
+        return DBConnectionUtils.getConnection();
+    }
+
+
 
 }
