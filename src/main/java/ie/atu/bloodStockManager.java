47a130f65a9f4e7803b839b16d3ddd470b11cd47
Patrick <g00409592@atu.ie>
2023-04-30 21:57:12 +0100

    Create bloodStockManager and a method called updateTable_blood_stock inside of it.

---
+++ src/main/java/ie/atu/bloodStockManager.java
@@ -0,0 +1,38 @@
+package ie.atu;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class bloodStockManager {
+    public static Connection getConnection() throws SQLException {
+        return DBConnectionUtils.getConnection();
+    }
+
+    public static boolean updateTable_blood_stock() {
+        String updateSQL = "UPDATE blood_stock bs " +
+                    "LEFT JOIN ( " +
+                    "SELECT blood_typesID, COUNT(*) AS count_units " +
+                    "FROM donated_blood " +
+                    "GROUP BY blood_typesID " +
+                    ") db ON bs.blood_type_id = db.blood_typesID " +
+                    "SET bs.amount = COALESCE(db.count_units, 0) " +
+                    "WHERE bs.blood_type_id BETWEEN 1 AND 8";
+
+        try (Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(updateSQL)) {
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+}
