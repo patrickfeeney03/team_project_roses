2e0ae1722f12f4d9a2f2770a8da95836a5869ced
Mikaela-D <G00401392@atu.ie>
2023-04-30 23:09:37 +0100

    Created new method selectBloodByDate

--- src/main/java/ie/atu/BloodUnitManager.java
+++ src/main/java/ie/atu/BloodUnitManager.java
@@ -1,4 +1,10 @@
 package ie.atu;
 
 public class BloodUnitManager {
+
+    // Method to select the blood to be donated with the closest expiration date
+    public String selectBloodByDate() {
+
+    }
+
 }
