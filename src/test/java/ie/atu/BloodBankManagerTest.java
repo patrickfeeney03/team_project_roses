68cd3480a5862f958052999d64bf6078f0130298
Mikaela-D <G00401392@atu.ie>
2023-04-30 13:02:39 +0100

    Created new method getBloodBankByID in BloodBankManagerTest

--- src/test/java/ie/atu/BloodBankManagerTest.java
+++ src/test/java/ie/atu/BloodBankManagerTest.java
@@ -1,4 +1,8 @@
 package ie.atu;
 
 public class BloodBankManagerTest {
+    public static void main(String[] args) {
+        //Select info from a bloodBank by using the ID
+        BloodBankbloodBankInfo = BloodBankManager.getBloodBankByID(0);
+    }
 }
