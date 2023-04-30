def5cd8ba815629afac162f65436ca49bcd1144e
Mikaela-D <G00401392@atu.ie>
2023-04-30 13:48:17 +0100

    Added new method to BloodBankManagerTest

--- src/test/java/ie/atu/BloodBankManagerTest.java
+++ src/test/java/ie/atu/BloodBankManagerTest.java
@@ -6,5 +6,8 @@ public class BloodBankManagerTest {
     public static void main(String[] args) {
         // Select info from a bloodBank by using the ID
         BloodBank bloodBankInfo = BloodBankManager.getBloodBankByID(0);
+
+        // Select info from all bloodBanks by using their ID
+        BloodBank allBloodBankInfo = BloodBankManager.getAllBloodBankByID(0);
     }
 }
