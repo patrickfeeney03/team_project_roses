4ba3e2aae886e4510f1131830e6a6c6b55eac77f
Mikaela-D <G00401392@atu.ie>
2023-05-01 12:59:50 +0100

    Fixed a bug

--- src/main/java/ie/atu/BloodUnitManager.java
+++ src/main/java/ie/atu/BloodUnitManager.java
@@ -11,12 +11,12 @@ public class BloodUnitManager {
         int amount = scanner.nextInt();
 
         // MySQL code to select blood from the blood bank with the closest expiration date
-    /*public String selectBloodByDate() {
+
         String selectBloodDate = "SELECT * FROM blood_units_date " +
         "WHERE DATE_ADD(blood_date, INTERVAL 2 MONTH) >= CURDATE() " +
         "ORDER BY DATEDIFF(DATE_ADD(blood_date, INTERVAL 2 MONTH), CURDATE()) ASC " +
         "LIMIT 5";
-    }*/
+
         return bloodStock;
     }
 }
