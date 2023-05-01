33f38eb8b41213208a23153dd93b133cc469a6c3
Jakub Mamala <G00408184@atu.ie>
2023-05-01 20:07:33 +0100

    Made the report look attractive

--- src/main/java/ie/atu/Report.java
+++ src/main/java/ie/atu/Report.java
@@ -3,26 +3,28 @@ package ie.atu;
 public class Report {
 
 
-    public void Donor_Report(){
+    public void Donor_Report(Donation donation){
 
         PatientManager patientManager = new PatientManager();
-        BloodType bloodType = new BloodType("A",'+');
-        Donor donor = new Donor(patientManager.getSinglePatientInfo(2),bloodType);
-        BloodBank bloodBank = new BloodBank(3,"dsa","sad","dsa");
-        Donation donation = new Donation(donor,bloodBank,5);
-
-        System.out.println(donation.getDonor().getBloodType().getBloodGroup());
+        System.out.println("******************************************************************\n\n\n\n\n\n\n\n\n");
+        System.out.println("Donor Report:");
+        System.out.println("Name: "+donation.getDonor().getPatient_firstName()+donation.getDonor().getPatient_lastName()+"\nD.O.B: "
+                +donation.getDonor().getPatient_DOB()+"\nEmail: "+donation.getDonor().getPatient_email()+"\nAddress: "
+                +donation.getDonor().getPatient_address()+"\nPhone: "+donation.getDonor().getPatient_phone()+"\nBlood type: "+donation.getDonor().getBloodType()+"\nUnits Donated: "
+                +donation.getUnitsDonated());
+        System.out.println("\n\n\n\n\n\n\n\n\n******************************************************************");
 
 
     }
-    public void Recipient_Report(){
-
+    public void Recipient_Report(Receive receive){
 
         PatientManager patientManager = new PatientManager();
-        BloodType bloodType = new BloodType("A",'+');
-        Recipient recipient = new Recipient(patientManager.getSinglePatientInfo(2),bloodType);
-        BloodBank bloodBank = new BloodBank(3,"dsa","sad","dsa");
-        Receive receive = new Receive(recipient,bloodBank,6);
+        System.out.println("******************************************************************\n\n\n\n\n\n\n\n\n");
+        System.out.println("Name: "+receive.getRecipient().getPatient_firstName()+receive.getRecipient().getPatient_lastName()+
+                "\nD.O.B: "+receive.getRecipient().getPatient_DOB()+"\nEmail: "+receive.getRecipient().getPatient_email()+"\nAddress: "
+                +receive.getRecipient().getPatient_address()+"\nPhone: "+receive.getRecipient().getPatient_phone()+"\nBlood type: "+receive.getRecipient().getBloodType()+"\nUnits Received: "
+                +receive.getUnitsReceived());
+        System.out.println("\n\n\n\n\n\n\n\n\n******************************************************************");
 
 
     }
