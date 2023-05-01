d7f1566489de803acdf933d70340e18901c8910e
Mikaela-D <G00401392@atu.ie>
2023-05-01 12:39:00 +0100

    Added new testing class BloodUnitManagerTest

--- src/main/java/ie/atu/BloodUnitManager.java
+++ src/main/java/ie/atu/BloodUnitManager.java
@@ -2,7 +2,7 @@ package ie.atu;
 
 // Method to select the blood to be donated with the closest expiration date
 public class BloodUnitManager {
-    // MySQL code to select blood from the bloodbank with the closest expiration date
+    // MySQL code to select blood from the blood bank with the closest expiration date
     public String selectBloodByDate() {
         String selectBloodDate = "SELECT * FROM blood_units_date " +
         "WHERE DATE_ADD(blood_date, INTERVAL 2 MONTH) >= CURDATE() " +
