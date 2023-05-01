a2dd52bc53ae73f794abe2838d895aebe14c1645
Mikaela-D <G00401392@atu.ie>
2023-05-01 12:57:59 +0100

    Created method getBestBlood

--- src/main/java/ie/atu/BloodUnitManager.java
+++ src/main/java/ie/atu/BloodUnitManager.java
@@ -1,13 +1,22 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 // Method to select the blood to be donated with the closest expiration date
 public class BloodUnitManager {
-    // MySQL code to select blood from the blood bank with the closest expiration date
-    public String selectBloodByDate() {
+
+    public static BloodStock getBestBlood() {
+        BloodStock bloodStock = null;
+        Scanner scanner = new Scanner(System.in);
+        int amount = scanner.nextInt();
+
+        // MySQL code to select blood from the blood bank with the closest expiration date
+    /*public String selectBloodByDate() {
         String selectBloodDate = "SELECT * FROM blood_units_date " +
         "WHERE DATE_ADD(blood_date, INTERVAL 2 MONTH) >= CURDATE() " +
         "ORDER BY DATEDIFF(DATE_ADD(blood_date, INTERVAL 2 MONTH), CURDATE()) ASC " +
         "LIMIT 5";
+    }*/
+        return bloodStock;
     }
-
 }
--- src/test/java/ie/atu/BloodUnitManagerTest.java
+++ src/test/java/ie/atu/BloodUnitManagerTest.java
@@ -1,4 +1,8 @@
 package ie.atu;
 
 public class BloodUnitManagerTest {
+    public static void main(String[] args) {
+        System.out.println("Enter the desired blood type");
+        BloodStock BestBlood = BloodUnitManager.getBestBlood();
+    }
 }
