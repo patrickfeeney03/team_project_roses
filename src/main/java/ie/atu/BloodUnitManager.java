5a35e4cef0f0d1aa77a92e1b1a4234f6aff8c6f5
Mikaela-D <G00401392@atu.ie>
2023-05-01 15:53:57 +0100

    Added a try catch to BloodUnitManager

--- src/main/java/ie/atu/BloodUnitManager.java
+++ src/main/java/ie/atu/BloodUnitManager.java
@@ -1,14 +1,23 @@
 package ie.atu;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 // Method to select the blood to be donated with the closest expiration date
 public class BloodUnitManager {
 
-    public static BloodStock getBestBlood() {
+    public static BloodStock getBestBloodByDate() {
         BloodStock bloodStock = null;
-        Scanner scanner = new Scanner(System.in);
-        int amount = scanner.nextInt();
+
+        /*Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the blood type ID : ");
+        int bloodGroup = scanner.nextInt();
+        System.out.println("Enter the amount of blood units : ");
+        int amount = scanner.nextInt();*/
+
 
         // MySQL code to select blood from the blood bank with the closest expiration date
 
@@ -17,6 +26,15 @@ public class BloodUnitManager {
         "ORDER BY DATEDIFF(DATE_ADD(blood_date, INTERVAL 2 MONTH), CURDATE()) ASC " +
         "LIMIT 5";
 
+        try (Connection connection = DBConnectionUtils.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(selectBloodDate)) {
+
+             ResultSet resultSet = preparedStatement.executeQuery();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return bloodStock;
     }
 }
--- src/test/java/ie/atu/BloodUnitManagerTest.java
+++ src/test/java/ie/atu/BloodUnitManagerTest.java
@@ -2,7 +2,6 @@ package ie.atu;
 
 public class BloodUnitManagerTest {
     public static void main(String[] args) {
-        System.out.println("Enter the desired blood type");
-        BloodStock BestBlood = BloodUnitManager.getBestBlood();
+        BloodStock BestBlood = BloodUnitManager.getBestBloodByDate();
     }
 }
